074359258c9151fdee9c450e680cfa26014080e6
Baljit <baljit.singh@suncorp.co.nz>
2016-04-18 17:21:39 +1200

    added view model

--- src/main/java/com/stock/controller/StockController.java
+++ src/main/java/com/stock/controller/StockController.java
@@ -1,6 +1,7 @@
 package com.stock.controller;
 
 import com.stock.domain.Stock;
+import com.stock.domain.StockViewModel;
 import com.stock.service.StockService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
@@ -38,13 +40,40 @@ public class StockController {
     }
 
     @RequestMapping(value = "/addStock" , method = RequestMethod.POST)
-    public void saveStock(@RequestBody Stock stock) {
+    public void saveStock(@RequestBody StockViewModel stockViewModel) {
 
-        System.out.println(stock.getName());
-        System.out.println(stock.getCode());
-        System.out.println(stock.getStockData().getPrice());
+        System.out.println(stockViewModel.getProductCategory());
+        System.out.println(stockViewModel.getProductCode());
+        System.out.println(stockViewModel.getProductName());
+        System.out.println(stockViewModel.getCostPrice());
+        System.out.println(stockViewModel.getRetailPrice());
+
+        System.out.println(stockViewModel.getProductDescription());
+        System.out.println(stockViewModel.getProductInvoiceDescription());
+
+        System.out.println(stockViewModel.getDiscountStartDate());
+        System.out.println(stockViewModel.getDiscountEndDate());
+        System.out.println(stockViewModel.getDiscountExpires());
+
+        System.out.println(stockViewModel.getDiscountType());
+
+        System.out.println(stockViewModel.getDiscountAmount());
+        System.out.println(stockViewModel.getDiscountPercentage());
+
+        System.out.println(stockViewModel.getDiscountForProduct());
+        System.out.println(stockViewModel.getDiscountAmountForOrdersOver());
+
+        SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd");
+
+        System.out.println("Current Date: " + ft.format(stockViewModel.getDiscountStartDate()));
+        System.out.println("End Date: " + ft.format(stockViewModel.getDiscountEndDate()));
+
+
+
         System.out.println(">>>>>>>>>>>>>>>>>");
     }
 
 
 }
+
+
---
+++ src/main/java/com/stock/domain/StockViewModel.java
@@ -0,0 +1,145 @@
+package com.stock.domain;
+
+import java.util.Date;
+
+/**
+ * Created by u342597 on 18/04/2016.
+ */
+public class StockViewModel {
+
+    private String productCategory;
+    private String productCode;
+    private String productName;
+    private String retailPrice;
+    private String productDescription;
+    private String productInvoiceDescription;
+    private String costPrice;
+    private String discountType;
+    private String discountAmount;
+    private String discountPercentage;
+    private String discountForProduct;
+    private String discountAmountForOrdersOver;
+    private Date discountStartDate;
+    private Date discountEndDate;
+    private String discountExpires;
+
+    public String getProductCategory() {
+        return productCategory;
+    }
+
+    public void setProductCategory(String productCategory) {
+        this.productCategory = productCategory;
+    }
+
+    public String getProductCode() {
+        return productCode;
+    }
+
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getCostPrice() {
+        return costPrice;
+    }
+
+    public void setCostPrice(String costPrice) {
+        this.costPrice = costPrice;
+    }
+
+    public String getDiscountAmount() {
+        return discountAmount;
+    }
+
+    public void setDiscountAmount(String discountAmount) {
+        this.discountAmount = discountAmount;
+    }
+
+    public Date getDiscountEndDate() {
+        return discountEndDate;
+    }
+
+    public void setDiscountEndDate(Date discountEndDate) {
+        this.discountEndDate = discountEndDate;
+    }
+
+    public String getDiscountExpires() {
+        return discountExpires;
+    }
+
+    public void setDiscountExpires(String discountExpires) {
+        this.discountExpires = discountExpires;
+    }
+
+    public String getDiscountForProduct() {
+        return discountForProduct;
+    }
+
+    public void setDiscountForProduct(String discountForProduct) {
+        this.discountForProduct = discountForProduct;
+    }
+
+    public String getDiscountAmountForOrdersOver() {
+        return discountAmountForOrdersOver;
+    }
+
+    public void setDiscountAmountForOrdersOver(String discountAmountForOrdersOver) {
+        this.discountAmountForOrdersOver = discountAmountForOrdersOver;
+    }
+
+    public String getDiscountPercentage() {
+        return discountPercentage;
+    }
+
+    public void setDiscountPercentage(String discountPercentage) {
+        this.discountPercentage = discountPercentage;
+    }
+
+    public Date getDiscountStartDate() {
+        return discountStartDate;
+    }
+
+    public void setDiscountStartDate(Date discountStartDate) {
+        this.discountStartDate = discountStartDate;
+    }
+
+    public String getDiscountType() {
+        return discountType;
+    }
+
+    public void setDiscountType(String discountType) {
+        this.discountType = discountType;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public String getProductInvoiceDescription() {
+        return productInvoiceDescription;
+    }
+
+    public void setProductInvoiceDescription(String productInvoiceDescription) {
+        this.productInvoiceDescription = productInvoiceDescription;
+    }
+
+    public String getRetailPrice() {
+        return retailPrice;
+    }
+
+    public void setRetailPrice(String retailPrice) {
+        this.retailPrice = retailPrice;
+    }
+}
