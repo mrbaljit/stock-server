501b133fa2bd56f50264de530d439830c5915d82
Baljit <baljit.singh@suncorp.co.nz>
2016-05-02 13:54:19 +1200

    added tester

--- src/main/java/com/product/repository/ProductRepository.java
+++ src/main/java/com/product/repository/ProductRepository.java
@@ -4,9 +4,16 @@ import com.product.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * Created by u342597 on 14/04/2016.
  */
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Long> {
+
+    List<Product> findByProductCode(String productCode);
+
+    List<Product> findByProductCodeContainingIgnoreCase(String productCode);
+
 }
--- src/test/java/com/example/ProductServerApplicationTests.java
+++ src/test/java/com/example/ProductServerApplicationTests.java
@@ -5,6 +5,9 @@ import com.product.domain.Product;
 import com.product.domain.ProductDiscount;
 import com.product.repository.ProductDiscountRepository;
 import com.product.repository.ProductRepository;
+import static org.junit.Assert.*;
+
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,7 +15,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = ProductServerApplication.class)
@@ -24,6 +29,8 @@ public class ProductServerApplicationTests {
 	@Autowired
 	ProductRepository productRepository;
 
+	Long productId;
+
 	@Before
 	public void setUp() {
 
@@ -34,15 +41,37 @@ public class ProductServerApplicationTests {
 		productDiscount.setDiscountTypes("1");
 
 		productDiscountRepository.save(productDiscount);
-		productRepository.save(new Product(1, "ADID", "Adidas Jersey",
+		productRepository.save(new Product(1, "DELETE", "test product",
 				"3", "XL Jersey Red Colour", "2010 XL Red",
-				"500", "600", productDiscount));
+				"100", "200", productDiscount));
+
+	}
+
+	@After
+	public void tearDown() {
+
+	    productRepository.delete(productId);
+
+	}
+
+
+	/*@Test
+	public void getAll() {
+		System.out.println(productRepository.findAll().size());
+	}*/
+
+	@Test
+	public void findByProductCode() {
+		List<Product> prd = productRepository.findByProductCode("DELETE");
+		productId = prd.get(0).getProductId();
 
 	}
 
 	@Test
-	public void getItemsShouldReturnBothItems() {
-		System.out.println(productRepository.findAll().size());
+	public void findByProductCodeContainingIgnoreCase() {
+		List<Product> prd = productRepository.findByProductCodeContainingIgnoreCase("delete");
+		productId = prd.get(0).getProductId();
+
 	}
 
 }
